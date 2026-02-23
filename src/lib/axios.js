8de19befe3b77015db5e35e88720d3f09bdfe282
sahillll0 <sahiltippe143@gmail.com>
2026-02-23 15:20:40 +0530

    new

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "https://site-check-apis-1y4fizc0n-sahillll0s-projects.vercel.app/api",
+    baseURL: import.meta.env.VITE_BACKEND_URL || "https://site-check-apis.vercel.app/api",
     withCredentials: true,
 });
