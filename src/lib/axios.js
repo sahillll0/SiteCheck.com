c49027a33b05ab2953458c84b280bbf50683818c
sahillll0 <sahiltippe143@gmail.com>
2026-02-24 13:27:54 +0530

    second

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:8001/api",
+    baseURL: "https://site-check-api.vercel.app/api",
     withCredentials: true,
 });
